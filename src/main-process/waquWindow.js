83d5be409c13de655c2fb4c11619c3cba821c458
Jakub Gajzler <jgajzler@bol.com>
2018-07-30 10:19:33 +0200

    Fixed view loading

--- src/main-process/waquWindow.js
+++ src/main-process/waquWindow.js
@@ -1,6 +1,7 @@
 const {BrowserWindow, app} = require('electron');
 const {EventEmitter} = require('events')
 const url = require('url');
+const path = require('path');
 // const ICON_PATH = path.resolve(__dirname, '..', '..', 'resources', 'atom.png')
 
 module.exports = class WaquWindow extends EventEmitter {
@@ -13,17 +14,19 @@ module.exports = class WaquWindow extends EventEmitter {
       title: 'Waqu',
       tabbingIdentifier: 'wacu',
       webPreferences: {
-        // backgroundThrottling: !this.isSpec,
+        backgroundThrottling: !this.isSpec,
         disableBlinkFeatures: 'Auxclick'
       }
     };
 
     this.browserWindow = new BrowserWindow(options);
+    //Only for debuging
+    this.browserWindow.webContents.openDevTools()
 
     this.browserWindow.loadURL(
       url.format({
         protocol: 'file',
-        pathname: '../../static/index.html',
+        pathname: path.join(__dirname, '../../static/index.html'),
         slashes: true
       })
     );
